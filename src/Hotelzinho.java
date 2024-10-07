02bef57b6cf4ee23deadadfef843e11a80856c02
miguelmn18 <miguel.macedo@dcx.ufpb.br>
2024-10-07 18:58:31 -0300

    repositorioPetShop

--- src/Hotelzinho.java
+++ src/Hotelzinho.java
@@ -1,11 +1,12 @@
-import javax.xml.crypto.Data;
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.Objects;
 
 public class Hotelzinho extends ServicoBase {
     int quantidadeDeHoras;
-    public Hotelzinho(LocalDate data, String codigo, String tamanhoAnimal, double preco, int quantidadeDeHoras) {
-        super(data, codigo, tamanhoAnimal, preco);
+
+    public Hotelzinho(String tamanhoAnimal, int preco, int quantidadeDeHoras) {
+        super(tamanhoAnimal, preco);
         this.quantidadeDeHoras = quantidadeDeHoras;
     }
 
@@ -16,11 +17,35 @@ public class Hotelzinho extends ServicoBase {
     public void setQuantidadeDeHoras(int quantidadeDeHoras) {
         this.quantidadeDeHoras = quantidadeDeHoras;
     }
-    public double getPreco(){
+    public int getPreco(){
         return 0;
     }
 
     public String getDescricao(){
         return null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Hotelzinho that = (Hotelzinho) o;
+        return quantidadeDeHoras == that.quantidadeDeHoras;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), quantidadeDeHoras);
+    }
+
+    @Override
+    public String toString() {
+        return "Hotelzinho{" + "Data= " + super.getData() +
+                "Código=" + super.getCodigo() +
+                "TamanhoAnimal=" + super.getTamanhoAnimal() +
+                "Preco=" + super.getPreco() +
+                "quantidadeDeHoras=" + quantidadeDeHoras +
+                '}';
     }
 }
